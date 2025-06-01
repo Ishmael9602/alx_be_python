6018028939faec5e0c24944a0f0baa34190572b4
Ishmael Mavundla <v7.vundla@gmail.com>
2025-06-01 19:28:16 +0200

    Add pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+# Ask user for pattern size (positive integer)
+size = int(input("Enter the size of the pattern: "))
+
+# Initialize row counter
+row = 0
+
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
