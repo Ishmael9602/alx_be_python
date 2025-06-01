8879536ddcaa1fb405b855834bd7d2b1eb0197b0
Ishmael Mavundla <v7.vundla@gmail.com>
2025-06-01 13:28:17 +0200

    Basic Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Declaring and Initializing Variables
+number1 = 10
+number2 = 5
+
+# Arithmatic operation
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results of the arithmatic operation
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
